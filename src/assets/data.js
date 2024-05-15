ae9b1ec7e85c0880a04c2e954cad288755c4d536
sreekant2x <sreekantsaha@gmaii.com>
2024-05-15 23:25:14 +0530

    fully completed

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+      question: "Which country has the longest coastline in the world?",
+      option1: "Canada",
+      option2: "Russia",
+      option3: "Australia",
+      option4: "India",
+      ans: 1,
+    },
+    {
+      question: "What is the value of π (pi) rounded to two decimal places?",
+      option1: "3.12",
+      option2: "3.16",
+      option3: "3.18",
+      option4: "3.14",
+      ans: 4,
+    },
+    {
+      question: "What is the chemical symbol for water?",
+      option1: "W",
+      option2: "H2O",
+      option3: "H2",
+      option4: "O2",
+      ans: 2,
+    },
+    {
+      question: "Question: What does HTML stand for?",
+      option1: "Hyper Tech Markup Language",
+      option2: "Hyperlinks and Text Markup Language",
+      option3: "Hyper Text Markup Language ",
+      option4: "Hyper Text Manipulation Language",
+      ans: 3,
+    },
+    {
+      question: "First page of Website is termed as?",
+      option1: "Index Page",
+      option2: "Homepage",
+      option3: "Sitemap",
+      option4: "Pen Drive",
+      ans: 2,
+    },
+  ];
